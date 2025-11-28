1a585e80b4d761f9df5e66ee62b22cceeb21c59c
Daya Pal <dayapal12pc@gmail.com>
2025-11-28 10:44:21 +0530

    fix the server.js file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 import express from "express";
-import "dotenv/config";
-import cors from "cors";
-import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest } from "./inngest/index.js";
-import * as functions from "./inngest/functions/index.js";  // FIX
+import { inngest } from "../inngest/index.js";
+import * as functions from "../inngest/functions/index.js";
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
+import serverless from "serverless-http";
 
 const app = express();
 
@@ -12,11 +12,9 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
-// Test route
 app.get("/", (req, res) => res.send("server is live!"));
 
-// Correct Inngest route
+// Inngest route
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`server is running on ${PORT}`));
+export default serverless(app); // IMPORTANT
