9a6f698f317783b2165c8b8928f12e91321d78c3
Daya Pal <dayapal12pc@gmail.com>
2025-11-28 10:39:36 +0530

    fix the inngest error on vercel

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import {clerkMiddleware} from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
+import { inngest } from "./inngest/index.js";
+import * as functions from "./inngest/functions/index.js";  // FIX
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
+// Test route
+app.get("/", (req, res) => res.send("server is live!"));
 
-app.get("/", (req,res) => res.send("server is live!"))
+// Correct Inngest route
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 3000;
